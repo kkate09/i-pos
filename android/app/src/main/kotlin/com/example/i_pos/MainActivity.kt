52204ce078a13dd9a2bb475edf82133da356cdef
kkate09 <karynmurray09@gmail.com>
2024-01-31 14:07:23 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/i_pos/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.i_pos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Blank Flutter App'),
+        ),
+        body: Center(
+          child: Text(
+            'Welcome to Flutter!',
+            style: TextStyle(fontSize: 20),
+          ),
+        ),
+      ),
+    );
+  }
+}
